bfc4646f200fce4bebca2d0a5eff732b7f840838
wagner <wagner_bregeon@hotmail.com>
2022-12-08 13:35:53 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega um usuario
+//$root = new Usuario();
+//$root->loadbyId(4);
+//echo $root;
 
-$root->loadbyId(4);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("user", "12345");
+
+echo $usuario;
 
 ?>
